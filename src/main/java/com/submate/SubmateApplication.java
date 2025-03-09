2b801be902eeb49fce3ea188b599d8f5eae83db6
Seungwon-Choi <bestwondev@gmail.com>
2025-03-09 20:06:40 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/submate/SubmateApplication.java
@@ -0,0 +1,13 @@
+package com.submate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/submate/SubmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
